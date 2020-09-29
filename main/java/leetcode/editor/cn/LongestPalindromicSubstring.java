9fbc3148389fcdec33167299386f697537b3d72d
汪梦奇 <wangmengqi@wondersgroup.com>
2020-09-29 16:19:07 +0800

    求最长字串

--- main/java/leetcode/editor/cn/LongestPalindromicSubstring.java
+++ main/java/leetcode/editor/cn/LongestPalindromicSubstring.java
@@ -21,12 +21,36 @@ package leetcode.editor.cn;
 public class LongestPalindromicSubstring {
     public static void main(String[] args) {
         Solution solution = new Solution();
+        String abcbadcaeggeacd = solution.longestPalindrome("abcba");
+        System.out.println(abcbadcaeggeacd);
     }
 }
 
 class Solution {
     public String longestPalindrome(String s) {
-
-        return null;
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        String ans = "";
+        // 字串长度的遍历
+        for (int l = 0; l < n; ++l) {
+            // 遍历字符串
+            for (int i = 0; i + l < n; ++i) {
+                int j = i + l;
+                // 字串长度位1
+                if (l == 0) {
+                    dp[i][j] = true;
+                // 字串长度位2
+                } else if (l == 1) {
+                    dp[i][j] = (s.charAt(i) == s.charAt(j));
+                // 字串长度大于3
+                } else {
+                    dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]);
+                }
+                if (dp[i][j] && l + 1 > ans.length()) {
+                    ans = s.substring(i, j + 1);
+                }
+            }
+        }
+        return ans;
     }
 }
