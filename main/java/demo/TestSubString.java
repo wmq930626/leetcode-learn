0f2c5a9e6d48fa4d4487d9b7e3b478c0913b0553
汪梦奇 <wangmengqi@wondersgroup.com>
2020-10-21 12:24:54 +0800

    求最大公共字串

--- main/java/demo/TestSubString.java
+++ main/java/demo/TestSubString.java
@@ -1,33 +1,42 @@
 package demo;
 
 /**
- * 判断一个字符串是不是字串
+ * 字符串匹配算法
  */
 public class TestSubString {
     public static void main(String[] args) {
         String parent = "hello world";
         String sub1 = "l";
         String sub2 = "llo wor";
-        String sub3 = " world";
+        String sub3 = " world hello";
         boolean isSub1 = isSub(parent, sub1);
         System.out.println(isSub1);
         boolean isSub2 = isSub(parent, sub2);
         System.out.println(isSub2);
         boolean isSub3 = isSub(parent, sub3);
         System.out.println(isSub3);
+        System.out.println(maxCommonSun(parent,sub1));
+        System.out.println(maxCommonSun(parent,sub2));
+        System.out.println(maxCommonSun(parent,sub3));
     }
 
-    public static  boolean isSub(String parent,String sub){
+    /**
+     * 判断一个字符串是不是字串
+     * @param parent
+     * @param sub
+     * @return
+     */
+    public static boolean isSub(String parent, String sub) {
         boolean isSub = false;
         for (int i = 0; i < parent.length(); i++) {
             isSub = true;
-            for (int j = 0 ; j < sub.length(); j++){
+            for (int j = 0; j < sub.length(); j++) {
                 // 如果父串遍历完了，字串还没遍历完，证明一定不是字串
-                if (i == parent.length()){
+                if (i == parent.length()) {
                     return false;
                 }
                 // 如果不相等就直接跳出循环
-                if (sub.charAt(j) != parent.charAt(i)){
+                if (sub.charAt(j) != parent.charAt(i)) {
                     isSub = false;
                     break;
                 }
@@ -37,10 +46,40 @@ public class TestSubString {
                 }
             }
             // 如果内层循环遍历完了还是true，就证明包含字串
-            if (isSub){
+            if (isSub) {
                 return true;
             }
         }
         return isSub;
     }
+
+    /**
+     * 求两个字符串的最大公共字串
+     * @param a
+     * @param b
+     * @return
+     */
+    public static String maxCommonSun(String a, String b) {
+        String maxSubStr = "";
+        int max_len = 0;
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = 0; j < b.length(); j++) {
+                if (a.charAt(i) == b.charAt(j)){
+                    int m , n;
+                    for (m = i, n = j; m < a.length() && n < b.length(); m++, n++) {
+                        if (a.charAt(m) != b.charAt(n)){
+                            break;
+                        }
+                        if (max_len < m - i + 1){
+                            max_len = m - i;
+                            maxSubStr = a.substring(i, m + 1);
+                        }
+                    }
+                }
+            }
+        }
+
+        return maxSubStr;
+    }
+
 }
